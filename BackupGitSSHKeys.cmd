f51d78b018eed7e019b17be6d4d3675845c9df91
Dan Rivera <daniel.dr.rivera@gmail.com>
2015-07-06 12:57:01 -0400

    Add backup and restore command files to USB for Git SSH Keys

---
+++ BackupGitSSHKeys.cmd
@@ -0,0 +1,2 @@
+copy %userprofile%\.ssh\daniel_dr_rivera_rsa D:\SecurityKeys\Git\
+copy %userprofile%\.ssh\daniel_dr_rivera_rsa.pub D:\SecurityKeys\Git\
---
+++ RestoreGitSSHKeys.cmd
@@ -0,0 +1,2 @@
+copy D:\SecurityKeys\Git\daniel_dr_rivera_rsa %userprofile%\.ssh\
+copy D:\SecurityKeys\Git\daniel_dr_rivera_rsa.pub %userprofile%\.ssh\
